5c7c05d0abec7a56dd434c15dc4054885b7071bf
心叶 <yelloxing@gmail.com>
2019-03-22 17:38:22 +0800

    callHandler

--- jsBridge.js
+++ jsBridge.js
@@ -8,10 +8,32 @@
   // 本地注册的方法集合
   var messageHandlers = {};
 
+  // 辅助变量(可以无视的部分)
+  var uniqueId = 0;
+
+  // 用以触发scheme的iframe
+  var messagingIframe = document.createElement('iframe');
+  messagingIframe.style.display = 'none';
+  document.documentElement.appendChild(messagingIframe);
+
   window.jsBridge = {
 
     // H5调用，通知原生调用对应的api
     "callHandler": function (handlerName, data, callback) {
+
+      var callback_id = "no-callback";
+
+      // 判断是否有回调函数
+      if (typeof callback === 'function') {
+
+        // 生成一个回调函数id
+        // 并将id和对应回调添加进入回调函数集合responseCallbacks中
+        callback_id = "callback_" + (uniqueId++) + "_" + new Date().valueOf();
+        responseCallbacks[callback_id] = callback;
+      }
+
+      // 触发scheme
+      messagingIframe.src = "webviewjsscheme:" + callback_id + "/" + handlerName + "?" + data;
 
     },
 
